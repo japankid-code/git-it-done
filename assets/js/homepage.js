8074aa49badfe0bdf6bf119fd87d7ed869e7be03
japankid-code <japankid.jake@gmail.com>
2021-03-28 09:46:24 -0500

    repos display with issue count

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 let userFormEl = document.getElementById("user-form");
 let nameInputEl = document.getElementById("username");
+let repoContainerEl = document.getElementById("repos-container");
+let repoSearchTerm = document.getElementById("repo-search-term");
 
 const getUserRepos = function(user) {
     // format the github api url
@@ -7,7 +9,7 @@ const getUserRepos = function(user) {
 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            renderRepos(data, user);
         })
   });
 }
@@ -24,5 +26,31 @@ const formSubmitHandler = function(e) {
     }
 }
 
+const renderRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = '';
+    repoSearchTerm.textContent = searchTerm;
+    for (let i = 0; i < repos.length; i++) {
+        // format repo name
+        let repoName = `${repos[i].owner.login}/${repos[i].name}`;
+        // create a container for each repo
+        let repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-rom justify-space-between align-center";
+        // create span ele to hold repo name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        repoEl.appendChild(titleEl);
+        repoContainerEl.appendChild(repoEl);
+
+        // display repo issues
+        let statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = `<i class='fas fa-times status-icon icon-danger'></i>${repos[i].open_issues_count} issue(s)`;  
+        } else {
+            statusEl.innerHTML = `<i class='fas fa-check-square status-icon icon-success'></i>`
+        }
+        repoEl.appendChild(statusEl);
+    }
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
